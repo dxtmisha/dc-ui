e275c9c78077b41169cbd19fc30a534a435e429a
dxtmisha <dxtmisha@gmail.com>
2021-11-06 15:12:36 +0700

    tailwindcss, icons, images

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: false,
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+  corePlugins: [
+    'padding'
+  ]
+}
