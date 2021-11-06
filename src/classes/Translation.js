3ee029c45404526bcb0edc28c92f350579ab2b20
dxtmisha <dxtmisha@gmail.com>
2021-11-06 17:48:48 +0700

    + Translation

---
+++ src/classes/Translation.js
@@ -0,0 +1,58 @@
+const LANGUAGE_DEFAULT = 'en-GB'
+
+export default class Translation {
+  static _language
+  static _translations = {}
+
+  static get (name) {
+    return this._translations?.[name] || name
+  }
+
+  static init (paths = {}) {
+    const lang = document.querySelector('html').getAttribute('lang') || LANGUAGE_DEFAULT
+
+    this._language = lang
+
+    return this.import(paths?.[lang])
+  }
+
+  /**
+   * @param {Function|[...Function]} path
+   * @returns {Promise<void>}
+   */
+  static async import (path) {
+    for (const item in (Array.isArray(path) ? path : [path])) {
+      this.add((await path[item]()).default)
+    }
+
+    return this
+  }
+
+  static add (translations) {
+    if (translations) {
+      if (typeof translations === 'string') {
+        try {
+          translations = JSON.parse(translations)
+        } catch (e) {
+          translations = undefined
+        }
+      }
+
+      if (typeof translations === 'object') {
+        Object.assign(this._translations, translations)
+      }
+    }
+
+    return this
+  }
+
+  static getByList (list) {
+    const data = {}
+
+    list.forEach(name => {
+      data[name] = Translation.get(name)
+    })
+
+    return data
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Translation from '@/classes/Translation'
+import App from './App.vue'
+
+import './main.scss'
+
+Translation.init({
+  'en-GB': [
+    () => import('./translations/dc-ui/en-GB.json'),
+    () => import('./translations/interactive-demo/en-GB.json')
+  ]
+})
+  .then(() =>
+    createApp(App)
+      .use(store)
+      .use(router)
+      .mount('#app')
+  )
