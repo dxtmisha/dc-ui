1f58661c236d51034b0de4206b4a6c81a94026ea
dxtmisha <dxtmisha@gmail.com>
2022-01-11 10:51:14 +0700

    Подготовка к DAvatar

---
+++ src/components/DAvatar/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { props } from '@/components/DAvatar/props'
+import useAdmin from '@/uses/useAdmin'
+
+export default {
+  name: 'DAvatar',
+  props,
+  setup (props, context) {
+    useAdmin('d-avatar')
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
