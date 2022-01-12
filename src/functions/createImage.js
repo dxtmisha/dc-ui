f1894a4690a9524d475efaecea39e1eb0d9e359e
dxtmisha <dxtmisha@gmail.com>
2022-01-12 12:26:10 +0700

    DAvatar: извинен вывод изображения из canvas

--- src/functions/createImage.js
+++ src/functions/createImage.js
@@ -1,5 +1,28 @@
 import getFileResult from '@/functions/getFileResult'
 
+const MAX_SIZE = 600
+
+const getSRC = function (src, image) {
+  if (
+    src instanceof File &&
+    (
+      image.naturalHeight > MAX_SIZE ||
+      image.naturalWidth > MAX_SIZE
+    )
+  ) {
+    const is = image.naturalWidth >= image.naturalHeight
+    const canvas = document.createElement('canvas').getContext('2d')
+
+    canvas.canvas.width = is ? MAX_SIZE : (image.naturalWidth / image.naturalHeight * MAX_SIZE)
+    canvas.canvas.height = is ? (image.naturalHeight / image.naturalWidth * MAX_SIZE) : MAX_SIZE
+    canvas.drawImage(image, 0, 0, canvas.canvas.width, canvas.canvas.height)
+
+    return canvas.canvas.toDataURL()
+  } else {
+    return image.src
+  }
+}
+
 export default function createImage (src) {
   return new Promise(resolve => {
     if (src) {
@@ -11,7 +34,7 @@ export default function createImage (src) {
           image,
           height: image.naturalHeight,
           width: image.naturalWidth,
-          src: image.src
+          src: getSRC(src, image)
         })
       }
 
