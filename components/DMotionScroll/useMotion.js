834b9fb81d0de1c25c6bdd822f8421e66fdaa85d
dxtmisha <dxtmisha@gmail.com>
2022-01-21 14:02:30 +0700

    DMotionScroll: исправления ошибка со скролл движения

--- components/DMotionScroll/useMotion.js
+++ components/DMotionScroll/useMotion.js
@@ -1,4 +1,4 @@
-import { computed, nextTick, onMounted, onUpdated, ref, watch } from 'vue'
+import { computed, nextTick, onBeforeUpdate, onMounted, onUpdated, ref, watch } from 'vue'
 
 export default function useMotion (
   id,
@@ -8,44 +8,40 @@ export default function useMotion (
   context,
   onScroll
 ) {
-  let block, top
-  // let distance = 0
-  let time
-  let el
+  let top, time
 
   const propPage = ref(props.page)
   const propItem = computed(() => document.querySelector(`.${id}[data-page="${propPage.value}"]`))
 
   const emit = () => context.emit('on-scroll', {
     value: propPage.value,
-    item: propItem.value // ,
-    // distance
+    item: propItem.value
   })
 
-  const update = (start = false) => {
-    /*
-    if (
-      propPage.value &&
-      propItem.value &&
-      (top >= 0 || start)
-    ) {
-      propElement.value.scrollTop = propItem.value.offsetTop + distance + propElement.value.scrollTop - top
-      top = -1
-    } else {
-      top = +0
+  const update = async () => {
+    await nextTick()
+
+    if (propItem.value) {
+      const distance = propItem.value.getBoundingClientRect().top - propElement.value.getBoundingClientRect().top
+      propElement.value.scrollTop += distance
     }
-     */
   }
 
-  const controlPage = () => {
-    if (!time) {
+  onScroll.value = event => {
+    if (
+      !scroll.value || (
+        event.$element !== propElement.value &&
+        event.$element !== props.element
+      )
+    ) {
+      event.$event.stop()
+    } else if (!time) {
       time = setTimeout(() => {
         const central = propElement.value.scrollTop + (propElement.value.clientHeight / 2)
         let focus
 
         scroll.value.querySelectorAll(`.${id}`).forEach(item => {
           if (item.offsetTop < central) {
-            el = item
             focus = item.dataset.page
           }
         })
@@ -59,79 +55,18 @@ export default function useMotion (
     }
   }
 
-  onScroll.value = event => {
-    if (
-      !scroll.value || (
-        event.$element !== propElement.value &&
-        event.$element !== props.element
-      )
-    ) {
-      event.$event.stop()
-    } else if (!block) {
-      controlPage()
+  watch(propPage, emit)
 
-      if (el) {
-        top = el.getBoundingClientRect().top
-      }
-    }
-    /*
-    if (
-      !scroll.value || (
-        event.$element !== propElement.value &&
-        event.$element !== props.element
-      )
-    ) {
-      event.$event.stop()
-    } else if (top < 0) {
-      top = 0
-    } else if (top < 1) {
-      clearTimeout(time)
-
-      time = setTimeout(() => {
-        let focus
-
-        const central = propElement.value.scrollTop + propElement.value.clientHeight / 2
-
-        scroll.value.querySelectorAll(`.${id}`).forEach(item => {
-          if (item.offsetTop < central) {
-            focus = item.dataset.page
-          }
-        })
-
-        if (focus) {
-          propPage.value = focus
-        }
-      }, 256)
-    }
-    */
-  }
-
-  watch(propPage, () => {
-    block = true
-    console.warn('propPage.value', propPage.value, el, top)
-    emit()
-    /*
-    top = propElement.value.scrollTop
-    distance = top - propItem.value.offsetTop
-    emit()
-     */
-
-    requestAnimationFrame(async () => {
-      await nextTick()
-      // update()
-    })
+  onMounted(update)
+  onBeforeUpdate(() => {
+    top = propItem.value.getBoundingClientRect().top
   })
-
-  onMounted(async () => {
-    await nextTick()
-    update(true)
-  })
-
   onUpdated(async () => {
-    await nextTick()
-    update()
+    const distance = top - propItem.value.getBoundingClientRect().top
 
-    // block = false
+    if (distance !== 0) {
+      propElement.value.scrollTop -= distance
+    }
   })
 
   return {
