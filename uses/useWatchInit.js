569b3a56450fc8ca9c656b041dcddfefdb46d103
dxtmisha <dxtmisha@gmail.com>
2022-03-10 21:39:08 +0700

    Create useWatchInit.js

---
+++ uses/useWatchInit.js
@@ -0,0 +1,12 @@
+import { watch } from 'vue'
+
+/**
+ * @param sources
+ * @param {Function} cb
+ * @param {Boolean} deep
+ * @returns {ref}
+ */
+export default function useWatchInit (sources, cb, deep) {
+  watch(sources, cb, { deep })
+  cb()
+}
