4afd209653e3ba1100619874a9a88284a6d31ca8
dxtmisha <dxtmisha@gmail.com>
2022-03-14 09:48:57 +0700

    Update getExp.js

--- functions/getExp.js
+++ functions/getExp.js
@@ -1,6 +1,6 @@
 export default function getExp (value, pattern = ':value') {
   const data = typeof value === 'string'
-    ? value.replace(/([[\]\\^$.?*+()])/, '\\$1')
+    ? value.replace(/([[\]\\^$.?*+()])/ig, '\\$1')
     : value
 
   return new RegExp(pattern.replace(':value', data), 'ig')
